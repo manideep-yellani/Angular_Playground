050589c2e2e8468c61d2687561d0512ca9f28dd6
manideep-yellani <manideep577@gmail.com>
2025-08-14 12:21:05 -0400

    flipkart test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Categories } from './components/categories/categories';
+import { Carousel } from './components/carousel/carousel';
+import { Main } from './components/main/main';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Categories,
+    Carousel,
+    Main,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
