aa53f4666ce0ea38c3be295eb51216fb892c806a
manideep-yellani <manideep577@gmail.com>
2025-08-27 15:39:01 -0400

    fixed ngzon config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeCrud } from "./components/employee-crud/employee-crud";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeCrud],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
