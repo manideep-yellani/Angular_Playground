2ebb2286c126cab08268f9e73df28789797086f8
manideep-yellani <manideep577@gmail.com>
2025-08-27 15:21:39 -0400

    made some config changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { Categories } from './components/categories/categories';
-import { Carousel } from './components/carousel/carousel';
-import { Main } from './components/main/main';
-import { Footer } from './components/footer/footer';
-import { A824 } from "./assignments/a8-24/a8-24";
-import { HttpDemo1 } from "./components/http-demo1/http-demo1";
-import { CommentList } from "./components/comment-list/comment-list";
-import { Products } from "./components/products/products";
-import { A825 } from "./assignments/a8-25/a8-25";
-import {EmployeeCrud } from "./components/employee-crud/employee-crud";
 
 @Component({
   selector: 'app-root',
-  imports: [
-    EmployeeCrud
-],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
