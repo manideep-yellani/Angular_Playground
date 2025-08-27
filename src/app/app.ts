c12419f954b269ec7825e66c28079b7400a09e25
manideep-yellani <manideep577@gmail.com>
2025-08-27 13:28:27 -0400

    added zones.js

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,31 +5,17 @@ import { Categories } from './components/categories/categories';
 import { Carousel } from './components/carousel/carousel';
 import { Main } from './components/main/main';
 import { Footer } from './components/footer/footer';
-import { Databinding } from "./components/databinding/databinding";
-import { FormsModule } from '@angular/forms';
-import { Directives } from "./components/directives/directives";
-import { TableCard } from "./components/table-card/table-card";
-import { A819 } from "./assignments/a8-19/a8-19";
-import { Demo1 } from "./components/demo1/demo1";
-import { A820EmployeeCrud } from "./assignments/a8-20-employee-crud/a8-20-employee-crud";
-import { MyModal } from "./components/my-modal/my-modal";
-import { ProductList } from "./components/product-list/product-list";
-import { A821 } from "./assignments/a8-21/a8-21";
-import { ParentDemo } from "./components/parent-demo/parent-demo";
-import { EmployeeCrud } from "./assignments/a8-22/employee-crud/employee-crud";
 import { A824 } from "./assignments/a8-24/a8-24";
 import { HttpDemo1 } from "./components/http-demo1/http-demo1";
 import { CommentList } from "./components/comment-list/comment-list";
 import { Products } from "./components/products/products";
 import { A825 } from "./assignments/a8-25/a8-25";
+import {EmployeeCrud } from "./components/employee-crud/employee-crud";
 
 @Component({
   selector: 'app-root',
   imports: [
-    HttpDemo1,
-    CommentList,
-    Products,
-    A825
+    EmployeeCrud
 ],
   templateUrl: './app.html',
   styleUrl: './app.css'
